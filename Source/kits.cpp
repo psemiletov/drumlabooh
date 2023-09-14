5b336f664a21f783d0288e799d9ac855f5372d9f
Petr Semiletov <peter.semiletov@gmail.com>
2023-09-14 21:36:07 +0300

    site upd

--- Source/kits.cpp
+++ Source/kits.cpp
@@ -478,7 +478,7 @@ std::string guess_sample_name (const std::string &raw)
 
 
 // trim from right
-inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+inline std::string& rtrim (std::string& s, const char* t = " \t\n\r\f\v")
 {
   s.erase(s.find_last_not_of(t) + 1);
   return s;
