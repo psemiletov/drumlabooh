0787f071b8967cdc927d41bbc9639c83f23e3646
Petr Semiletov <peter.semiletov@gmail.com>
2025-01-14 19:20:49 +0200

    + drumkit format extended

--- Source/kits.cpp
+++ Source/kits.cpp
@@ -512,7 +512,9 @@ void CDrumKit::load_txt (const std::string &data)
          bool check_for_random_noice = false;
          bool check_for_txt = false;
                   
-         if (fname.find ("samples.txt") != string::npos || fname.find (".part") != string::npos)
+         //if (fname.find ("samples.txt") != string::npos || fname.find (".part") != string::npos)
+           //  check_for_txt = true;
+         if (fname.find (".txt") != string::npos)
              check_for_txt = true;
          
          if (sample_name.rfind ("*", 0) == 0) 
